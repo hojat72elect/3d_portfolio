73c52fe5babbd0722fbe5fd043005f06a814a572
hojat <hojat72elect@gmail.com>
2023-05-19 14:34:51 -0400

    Completed skeleton of the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-function App() {
+import {BrowserRouter} from "react-router-dom";
+import {Navbar, Hero, About, Experience, Tech, Works, Feedbacks, Contact, StarsCanvas} from "./components/index.js";
 
+
+const App = () => {
     return (
-        <div>
-            Hojat 3D portfolio
-        </div>
+        <BrowserRouter>
+            <div className="relative z-0 bg-primary">
+                <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+                    <Navbar/>
+                    <Hero/>
+                </div>
+
+                <About/>
+                <Experience/>
+                <Tech/>
+                <Works/>
+                <Feedbacks/>
+
+                <div className="relative z-0">
+                    <Contact/>
+                    <StarsCanvas/>
+                </div>
+            </div>
+        </BrowserRouter>
     )
+
 }
 
 export default App
